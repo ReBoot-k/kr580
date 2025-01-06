208a1e98821cc98d8e90b60c67fe5f0fa9de301f
Михаил <rebootik@mail.ru>
2025-01-06 21:44:04 +0300

    update registers.ts
    Изменена концепция регистров

--- src/scripts/KR580CPU.ts
+++ src/scripts/KR580CPU.ts
@@ -1,3 +1,24 @@
-class KR580CPU {
+import { Register, RegisterPair } from "./registers.ts";
+
+const MAX_SIZE: number = 0xFFFF;
+
+class KR580CPU { 
+    private A: Register = new Register();
+    private BC: RegisterPair = new RegisterPair();
+    private DE: RegisterPair = new RegisterPair();
+    private HL: RegisterPair = new RegisterPair();
     
+    private PC: number = 0;
+    private SP: number = MAX_SIZE;
+    private PSW: number = 0;
+
+    private flags: Record<string, Boolean> = {
+        S:  false,
+        Z:  false,
+        AC: false,
+        P:  false,
+        CY: false
+    }
+
+    private memory: Array<number> = Array(MAX_SIZE).fill(0);
 }
--- src/scripts/registers.ts
+++ src/scripts/registers.ts
@@ -1,43 +1,60 @@
-class Registers {
-    // TODO: control input registers (filter, only: A, B, C, D, E, H, L, BC, DE, HL)
+export {Register, RegisterPair};
 
-    /* Example:
-        Register.setValue("A", 66);
-        Register.getValue("BC");
-    */
-    private registers: Record<string, number> = {
-        "A": 0,
-        "B": 0,
-        "C": 0,
-        "D": 0,
-        "E": 0,
-        "H": 0,
-        "L": 0,
+/*
+const A = new Register();
+A.value = 9;
+A.value; // значение равно 9
+
+const BC = new RegisterPair(0, 0); // аргументы не обязательны
+BC.high // первые 8 бит (регистр B)
+BC.low // последние 8 бит (регистр C)
+*/
+
+class Register {
+    private register: number = 0;
+
+    get value(): number {
+        return this.register;
     }
 
-    private limit(value: number, bits: number = 8): number {
-        const maximum = (1 << bits);
-        return value % maximum;
-    }
-
-    public getValue(register: string): number {
-        if (register.length == 1) {
-            return this.registers[register];
-        } else {
-            let registerFirst = register[0];
-            let registerSecond = register[1];
-            return (this.registers[registerFirst] << 8) | this.registers[registerSecond];
-        }
-    }
-
-    public setValue(register: string, value: number) {
-        if (register.length == 1) {
-            this.registers[register] = this.limit(value);
-        } else {
-            let registerFirst = register[0];
-            let registerSecond = register[1];
-            this.registers[registerFirst] = this.limit(value) >> 8;
-            this.registers[registerSecond] = this.limit(value);
-        }
+    set value(val: number) {
+        this.register = val & 0xFF;
     }
 }
+
+
+class RegisterPair {
+    private _low: number;
+    private _high: number;
+
+    constructor(high: number = 0, low: number = 0) {
+        this._high = high;
+        this._low = low;
+    }
+
+    get low(): number {
+        return this._low;
+    }
+
+    set low(value: number) {
+        this._low = value & 0xFF;
+    }
+
+    get high(): number {
+        return this._high;
+    }
+
+    set high(value: number) {
+        this._high = (value >> 8) & 0xFF;
+    }
+
+    get value(): number {
+        return (this._high << 8) | this._low;
+    }
+
+    set value(val: number) {
+        this._low = val & 0xFF;
+        this._high = (val >> 8) & 0xFF;
+    }
+}
+
